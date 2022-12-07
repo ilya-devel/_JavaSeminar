0c27f3be2b81222cbe6e2eb71b005b6fda5065ea
Ilya.devel <ilya.devel@yandex.ru>
2022-12-07 07:09:01 +0500

    added sem1 tsk1

--- src/sem1/tsk1.java
+++ src/sem1/tsk1.java
@@ -3,6 +3,23 @@ package sem1;
 
 public class tsk1 {
     public static void main(String[] args) {
+        System.out.println(addStrings("123", "23"));
+    }
 
+    public static String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1, j = num2.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int n1 = (i < 0) ? 0 : num1.charAt(i) - '0';
+            int n2 = (j < 0) ? 0 : num2.charAt(j) - '0';
+            int res = n1 + n2 + carry;
+            sb.append(res % 10);
+            carry = res / 10;
+            i--;
+            j--;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
     }
 }
